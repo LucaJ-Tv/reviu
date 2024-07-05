13af813c80b181e0eb2873ee0f2a61cc778ee5ac
LucaFerar <luca.ferartofan@studio.unibo.it>
2024-07-05 18:01:03 +0200

    add: config colors and logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "site-primary": "rrgb(248, 249, 251)",
+        "site-secondary": "rgb(255, 227, 0)",
+        "site-tertiary": "rgb(0, 182, 250)",
+        "site-error": "#D73A49",
+      }
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+      Inter: ["Inter, sans-serif"]
+    },
+    container:{
+      padding: "2rem",
+      center: true
+    }
   },
   plugins: [],
 }
-
