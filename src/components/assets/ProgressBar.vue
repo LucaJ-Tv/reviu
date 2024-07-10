3c32813b210a599241d0db612bc2dedc4fd22285
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-07-10 17:01:04 +0200

    add: ProgressBar, first draft of Vision

---
+++ src/components/assets/ProgressBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-8 h-full bg-gray-200 rounded-full overflow-hidden">
+      <div class="bg-site-tertiary w-full h-full" :style="{ height: progress + '%' }"></div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  
+  export default defineComponent({
+    name: 'ProgressBar',
+    props: {
+      progress: {
+        type: Number as PropType<number>,
+        required: true,
+      },
+    },
+  });
+  </script>
+  
+  
