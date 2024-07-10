4b87015068721c07aedc584ab46ce8a57394ecf8
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-07-10 16:09:28 +0200

    edit: notFoundView title

--- src/views/notFoundView.vue
+++ src/views/notFoundView.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="notFound flex flex-col h-screen justify-between text-gray-200 bg-site-primary">
       <Navbar :pageSelected="'0'"></Navbar>
-      <div class="text-black">RITORNA ALLA PAGINA INIZIALE</div>
+      <div class="flex flex-col h-screen justify-between bg-site-primary bg-opacity-35 backdrop-blur-md shadow-md border-gray-200">
+        <div class="flex flex-col items-center justify-center flex-1">
+            <h1 class="text-3xl font-bold text-center text-gray-800 mb-4">Pagina non trovata</h1>
+            <h2 class="text-xl font-bold text-center text-gray-800 mb-4">Torna alla schemata iniziale</h2>
+        </div>
+    </div>
       <Footer></Footer>
     </div>
   </template>
